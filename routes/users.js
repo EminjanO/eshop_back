fec55e25c90eafcef962ccc24ec393180b29c9d4
eminjano <emindilbar@gmail.com>
2022-01-03 00:49:01 +0100

    user uncommented...

--- routes/users.js
+++ routes/users.js
@@ -43,38 +43,38 @@ router.post('/', async (req,res)=>{
     res.send(user);
 })
 
-// router.put('/:id',async (req, res)=> {
+router.put('/:id',async (req, res)=> {
 
-//     const userExist = await User.findById(req.params.id);
-//     let newPassword
-//     if(req.body.password) {
-//         newPassword = bcrypt.hashSync(req.body.password, 10)
-//     } else {
-//         newPassword = userExist.passwordHash;
-//     }
+    const userExist = await User.findById(req.params.id);
+    let newPassword
+    if(req.body.password) {
+        newPassword = bcrypt.hashSync(req.body.password, 10)
+    } else {
+        newPassword = userExist.passwordHash;
+    }
 
-//     const user = await User.findByIdAndUpdate(
-//         req.params.id,
-//         {
-//             name: req.body.name,
-//             email: req.body.email,
-//             passwordHash: newPassword,
-//             phone: req.body.phone,
-//             isAdmin: req.body.isAdmin,
-//             street: req.body.street,
-//             apartment: req.body.apartment,
-//             zip: req.body.zip,
-//             city: req.body.city,
-//             country: req.body.country,
-//         },
-//         { new: true}
-//     )
+    const user = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            email: req.body.email,
+            passwordHash: newPassword,
+            phone: req.body.phone,
+            isAdmin: req.body.isAdmin,
+            street: req.body.street,
+            apartment: req.body.apartment,
+            zip: req.body.zip,
+            city: req.body.city,
+            country: req.body.country,
+        },
+        { new: true}
+    )
 
-//     if(!user)
-//     return res.status(400).send('the user cannot be created!')
+    if(!user)
+    return res.status(400).send('the user cannot be created!')
 
-//     res.send(user);
-// })
+    res.send(user);
+})
 
 router.post('/login', async (req,res) => {
     const user = await User.findOne({email: req.body.email})
@@ -100,26 +100,26 @@ router.post('/login', async (req,res) => {
 })
 
 
-// router.post('/register', async (req,res)=>{
-//     let user = new User({
-//         name: req.body.name,
-//         email: req.body.email,
-//         passwordHash: bcrypt.hashSync(req.body.password, 10),
-//         phone: req.body.phone,
-//         isAdmin: req.body.isAdmin,
-//         street: req.body.street,
-//         apartment: req.body.apartment,
-//         zip: req.body.zip,
-//         city: req.body.city,
-//         country: req.body.country,
-//     })
-//     user = await user.save();
+router.post('/register', async (req,res)=>{
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    })
+    user = await user.save();
 
-//     if(!user)
-//     return res.status(400).send('the user cannot be created!')
+    if(!user)
+    return res.status(400).send('the user cannot be created!')
 
-//     res.send(user);
-// })
+    res.send(user);
+})
 
 
 router.delete('/:id', (req, res)=>{
